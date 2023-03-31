1a80ab10d1d30ce90227c279dcd2926e7314cb6a
Jason Golliet <jgolliet@LABA.PSWAS.LOCAL>
2023-03-31 13:43:08 -0700

    first commit

---
+++ database_functions.js
@@ -0,0 +1,11 @@
+const getProducts = () => {
+    console.log("getting products from database")
+
+}
+
+const getSingleProduct = () => {
+    console.log("get one product from database")
+}
+
+getProducts()
+getSingleProduct()
